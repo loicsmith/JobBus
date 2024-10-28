38c55f3e23543bb0ec846ce05de4cbb8de07d780
Loicsmith <loicramond2007@gmail.com>
2024-10-28 12:06:30 +0100

    Ajout référence et début du main

--- Main.cs
+++ Main.cs
@@ -1,18 +1,45 @@
 ﻿using Life;
-using Life.AreaSystem;
-using Life.DB;
+using Life.BizSystem;
 using Life.Network;
-using System;
-
+using ModKit.Helper;
+using ModKit.Interfaces;
+using ModKit.Internal;
+using System.Collections.Generic;
+using _menu = AAMenu.Menu;
 
 namespace Main
 {
-    class Main : Plugin
+    class Main : ModKit.ModKit
     {
         public Main(IGameAPI api) : base(api)
         {
-
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Loicsmith");
         }
 
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+
+            InitAAmenu();
+        }
+
+        public void InitAAmenu()
+        {
+            _menu.AddBizTabLine(PluginInformations, new List<Activity.Type> { Activity.Type.Bus }, null, "Configuration SAE", (ui) =>
+            {
+                Player player = PanelHelper.ReturnPlayerFromPanel(ui);
+            });
+
+            _menu.AddBizTabLine(PluginInformations, new List<Activity.Type> { Activity.Type.Bus }, null, "Vente de titres de transports", (ui) =>
+            {
+                Player player = PanelHelper.ReturnPlayerFromPanel(ui);
+            });
+
+            _menu.AddAdminTabLine(PluginInformations, 5, "Configuration Lignes de bus", (ui) =>
+            {
+                Player player = PanelHelper.ReturnPlayerFromPanel(ui);
+            });
+        }
     }
 }
