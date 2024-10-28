23da148328c2c0871fc8a4adacfd311bf1e72fb8
Loicsmith <loicramond2007@gmail.com>
2024-10-28 11:13:58 +0100

    Ajoutez des fichiers projet.

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using Life;
+using Life.AreaSystem;
+using Life.DB;
+using Life.Network;
+using System;
+
+
+namespace Main
+{
+    class Main : Plugin
+    {
+        public Main(IGameAPI api) : base(api)
+        {
+
+        }
+
+    }
+}
